e3223d9e9b1ee8052754d29b6ca7f967f7d9785a
vvinayakkk <ntpjc2vinayak@gmail.com>
2024-07-15 01:13:11 +0530

    changes added

--- server/studybuddy/connections/serializers.py
+++ server/studybuddy/connections/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from authentication.models import User
 from .models import FriendRequest, Message
 
 class FriendRequestSerializer(serializers.ModelSerializer):
