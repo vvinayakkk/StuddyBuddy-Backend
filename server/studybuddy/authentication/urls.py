14947afd4fed8b9607e6890b4ec476ff349ca7d6
vvinayakkk <ntpjc2vinayak@gmail.com>
2024-07-16 05:53:54 +0530

    ...

--- server/studybuddy/authentication/urls.py
+++ server/studybuddy/authentication/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
    path('profile/', views.profile_view_get, name='profile_get'),
     path('profile/update/', views.profile_view_put, name='profile_put'),
     path('profile/image/', views.profile_image_update_view, name='profile_image_update'),
-    path('profile/image2/', views.profile_view_get, name='profile_image_update2'),
+    path('profile/image2/', views.profile_view_get2, name='profile_view_get2'),
     path('accept_friend_request/<int:request_id>/', views.accept_friend_request_view, name='accept_friend_request'),
     path('decline_friend_request/<int:request_id>/', views.decline_friend_request_view, name='decline_friend_request'),
 ]
